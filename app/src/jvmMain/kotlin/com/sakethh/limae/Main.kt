ab43c9f7f0407f93ae94ad7efa13766885327313
sakethpathike <sakethpathike@gmail.com>
2026-02-21 02:40:21 +0530

    feat(desktop): set minimum window size

--- app/src/jvmMain/kotlin/com/sakethh/limae/Main.kt
+++ app/src/jvmMain/kotlin/com/sakethh/limae/Main.kt
@@ -4,12 +4,14 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.sakethh.limae.ui.Limae
 import com.sakethh.limae.ui.theme.LimaeTheme
+import java.awt.Dimension
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Limae",
     ) {
+        this.window.minimumSize = Dimension(800, 600)
         LimaeTheme(darkTheme = true) {
             Limae()
         }
